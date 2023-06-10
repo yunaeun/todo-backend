52602a4a3690fffbe9ac39a8d82b227d416436c6
yunaeun <naeun2964@naver.com>
2023-06-10 23:43:39 +0900

    prod 환경조성

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.mariadb.jdbc:mariadb-java-client:2.1.2")
+    
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
